b062863361b70b62636b8c2ac546339db65cef03
JeorgeReds78 <69481052+JeorgeReds78@users.noreply.github.com>
2021-05-24 06:21:29 -0500

    Agregado Main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+Actividad Repaso OOP
+Proyecto desarrollado por Sergio Gonzalez y Jorge Rojas
+Actividad 2 Diseño Orienado a Objetos
+*/
+
+//Codigo Main creado por Jorge Rojas
+
+
+#include <iostream>
+
+using namespace std;
+
+//Variables 
+int seleccion;
+
+//Codigo Principal
+int main() {
+    return 0;
+}
