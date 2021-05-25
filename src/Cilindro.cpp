91a1d358172dcd0609376365cde0cdbdaade2e9b
JeorgeReds78 <69481052+JeorgeReds78@users.noreply.github.com>
2021-05-24 22:46:06 -0500

    cambie Cilindro y SObre

--- src/Cilindro.cpp
+++ src/Cilindro.cpp
@@ -4,12 +4,32 @@
 	
 using std::string;
 
-Cilindro::Cilindro() {
-    radio = 1.0;
-    altura = 1.0;
-}
-
-Cilindro::Cilindro(double radio, double altura){
+Cilindro::Cilindro(string paqueteria, string salida, string color,
+        string de,
+        string para, string direccion_destinatario,
+		string id_paquete, double peso,
+		string fecha_envio, string notas,
+        double radio, double altura){
     this-> radio = radio;
     this-> altura = altura;
+}
+
+void Cilindro::set_radio(double radio) {
+    this-> radio = radio;
+}
+
+void Cilindro::set_alto(double altura) {
+    this-> altura = altura;
+}
+
+void Cilindro::pedidoFinal() {
+    cout << "Informacion de Pedido: " << endl;
+    cout << "radio" << get_radio() << endl;
+    cout << "altura" << get_alto() << endl;
+
+
+    
+
+    
+
 }
--- src/Sobre.cpp
+++ src/Sobre.cpp
@@ -4,14 +4,39 @@
 	
 using std::string;
 
-Sobre::Sobre() {
-    largo = 1.0;
-    ancho = 1.0;
-    estampas = 2;
-}
 
-Sobre::Sobre(double largo, double ancho, int estampas){
+Sobre::Sobre(string paqueteria, string salida, string color,
+        string de,
+        string para, string direccion_destinatario,
+		string id_paquete, double peso,
+		string fecha_envio, string notas, 
+        double largo, double ancho, int estampas){
     this-> largo = largo;
     this-> ancho = ancho;
     this-> estampas = estampas;
+}
+
+void Sobre::set_largo(double largo) {
+    this-> largo = largo;
+}
+
+void Sobre::set_ancho(double ancho) {
+    this-> ancho = ancho;
+}
+
+void Sobre::set_estampas(double estampas) {
+    this-> estampas = estampas;
+}
+
+void Sobre::pedidoFinal() {
+    cout << "Informacion de Pedido: " << endl;
+    cout << "largo" << get_largo() << endl;
+    cout << "ancho" << get_ancho() << endl;
+    cout << "estampas" << get_estampas() << endl;
+
+
+    
+
+    
+
 }
